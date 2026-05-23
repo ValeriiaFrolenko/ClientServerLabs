4e09aad44802b313a1e0f40366d1f47388b5897c
Valeriia Frolenko <valeriiiafrol@gmail.com>
2026-05-24 00:39:17 +0300

    done practice 2

---
+++ Practice2/src/main/java/org/example/Data.java
@@ -0,0 +1,49 @@
+package org.example;
+
+public class Data {
+
+    private int state = 1;
+
+    public int getState() {
+        return state;
+    }
+
+    public synchronized void Tic() {
+        while (state != 1) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.print("Tic-");
+        state = 2;
+        notifyAll();
+    }
+
+    public synchronized void Tak() {
+        while (state != 2) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.print("Tak-");
+        state = 3;
+        notifyAll();
+    }
+
+    public synchronized void Toy(){
+        while (state != 3) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println("Toy");
+        state = 1;
+        notifyAll();
+    }
+}
